03aaeffa7087187213d6f0928fdc9a559e5a25b6
rnzid <ranjitdangol13@gmail.com>
2021-07-01 23:28:55 +0545

    category form added

--- WebSite/Controllers/CategoryController.cs
+++ WebSite/Controllers/CategoryController.cs
@@ -3,13 +3,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebSite.Data;
+using WebSite.Models;
 
 namespace WebSite.Controllers
 {
     public class CategoryController : Controller
     {
+        public readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            IEnumerable<Category> objList = _db.category;
+            return View(objList);
+        }
+
+        //Get-Create
+        public IActionResult Create()
+        {
+            
             return View();
         }
     }
